95fcfd127232fe7843fde29a57334ab64992abb0
nagisaShiyoto <elon.gilad9@gmail.com>
2024-12-14 01:19:14 +0200

    added the next thing i want to do

--- src.py
+++ src.py
@@ -2,10 +2,10 @@ from network import network
 from layer import layer_none
 import numpy as np
 def main():
+    #todo: add the mnist database
     network_test=network()
     img=np.random.randn(784)
     print(network_test.get_output(img=img))
-    print("hi")
 
 
 if __name__ == "__main__":
